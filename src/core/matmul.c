88146ffedf39465cd50e6cc7533d5f58286282e3
OMG_link <50469469+OMG-link@users.noreply.github.com>
2025-05-16 00:17:27 +0800

    feat: adjust blocking strategy

--- src/core/matmul.c
+++ src/core/matmul.c
@@ -67,22 +67,44 @@ void matmul_submat(f64 *dst, const f64 *lhs, const f64 *rhs, int m, int k, int n
         int n_idx;
         for (n_idx = 0; n_idx + DST_N_BLK <= n; n_idx += DST_N_BLK) {
 #ifdef __AVX512F__
-            __m512d out[DST_M_BLK];
-            for (int i = 0; i < DST_M_BLK; i++) {
-                out[i] = _mm512_loadu_pd(&dst[(m_idx + i) * dst_line_stride + n_idx]);
-            }
+            const double *lhs_val_ptr = lhs + m_idx * k;
+            const double *rhs_vec_ptr = rhs + n_idx * k;
+            double *dst_ptr = dst + m_idx * dst_line_stride + n_idx;
+            __m512d out00, out10, out20, out30, out40, out50, out60, out70;
+            __m512d out01, out11, out21, out31, out41, out51, out61, out71;
+
+#define load_out(i, j) out##i##j = _mm512_loadu_pd(dst_ptr + i * dst_line_stride + j * 8)
+            load_out(0, 0), load_out(1, 0), load_out(2, 0), load_out(3, 0);
+            load_out(4, 0), load_out(5, 0), load_out(6, 0), load_out(7, 0);
+            load_out(0, 1), load_out(1, 1), load_out(2, 1), load_out(3, 1);
+            load_out(4, 1), load_out(5, 1), load_out(6, 1), load_out(7, 1);
+#undef load_out
             for (int k_idx = 0; k_idx < k; k_idx++) {
-                for (int i = 0; i < DST_M_BLK; i++) {
-                    double lhs_val = lhs[m_idx * k + k_idx * DST_M_BLK + i];
-                    __m512d lhs_broadcast = _mm512_set1_pd(lhs_val);
-                    const double *rhs_vec_ptr = &rhs[n_idx * k + k_idx * DST_N_BLK];
-                    __m512d rhs_vec = _mm512_loadu_pd(rhs_vec_ptr);
-                    out[i] = _mm512_fmadd_pd(lhs_broadcast, rhs_vec, out[i]);
-                }
+                __m512d rhs_vec0 = _mm512_loadu_pd(rhs_vec_ptr + 0);
+                __m512d rhs_vec1 = _mm512_loadu_pd(rhs_vec_ptr + 8);
+                __m512d lhs_vbc0 = _mm512_set1_pd(lhs_val_ptr[0]);
+                __m512d lhs_vbc1 = _mm512_set1_pd(lhs_val_ptr[1]);
+                __m512d lhs_vbc2 = _mm512_set1_pd(lhs_val_ptr[2]);
+                __m512d lhs_vbc3 = _mm512_set1_pd(lhs_val_ptr[3]);
+                __m512d lhs_vbc4 = _mm512_set1_pd(lhs_val_ptr[4]);
+                __m512d lhs_vbc5 = _mm512_set1_pd(lhs_val_ptr[5]);
+                __m512d lhs_vbc6 = _mm512_set1_pd(lhs_val_ptr[6]);
+                __m512d lhs_vbc7 = _mm512_set1_pd(lhs_val_ptr[7]);
+#define fma(i, j) out##i##j = _mm512_fmadd_pd(lhs_vbc##i, rhs_vec##j, out##i##j)
+                fma(0, 0), fma(1, 0), fma(2, 0), fma(3, 0);
+                fma(4, 0), fma(5, 0), fma(6, 0), fma(7, 0);
+                fma(0, 1), fma(1, 1), fma(2, 1), fma(3, 1);
+                fma(4, 1), fma(5, 1), fma(6, 1), fma(7, 1);
+#undef fma
+                lhs_val_ptr += DST_M_BLK;
+                rhs_vec_ptr += DST_N_BLK;
             }
-            for (int i = 0; i < DST_M_BLK; i++) {
-                _mm512_storeu_pd(&dst[(m_idx + i) * dst_line_stride + n_idx], out[i]);
-            }
+#define store_out(i, j) _mm512_storeu_pd(dst_ptr + i * dst_line_stride + j * 8, out##i##j)
+            store_out(0, 0), store_out(1, 0), store_out(2, 0), store_out(3, 0);
+            store_out(4, 0), store_out(5, 0), store_out(6, 0), store_out(7, 0);
+            store_out(0, 1), store_out(1, 1), store_out(2, 1), store_out(3, 1);
+            store_out(4, 1), store_out(5, 1), store_out(6, 1), store_out(7, 1);
+#undef store_out
 #else
             outer_product_kernel(DST_M_BLK, DST_N_BLK);
 #endif
