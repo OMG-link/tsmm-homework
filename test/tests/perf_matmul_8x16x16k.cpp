a267e5315993e17d0c49d8a8427a366b46ef8ba4
OMG_link <50469469+OMG-link@users.noreply.github.com>
2025-05-20 14:08:49 +0800

    test: use perf intrinsic

--- test/tests/perf_matmul_8x16x16k.cpp
+++ test/tests/perf_matmul_8x16x16k.cpp
@@ -4,6 +4,7 @@
 
 #include "core/kernels/8x16x16000.hpp"
 #include "matmul.hpp"
+#include "perf.h"
 
 void test(const Matrix &a, const Matrix &b) {
     using namespace std::chrono;
@@ -11,18 +12,80 @@ void test(const Matrix &a, const Matrix &b) {
     MatMul8x16x16000 kernel;
     Matrix c(a.rows(), b.cols());
 
+    int fd_cycles = perf_event_cycles();
+    int fd_instrs = perf_event_instructions();
+    int fd_clock = perf_event_task_clock();
+    int fd_faults = perf_event_page_faults();
+    int fd_dtlb_a = perf_event_dtlb_access();
+    int fd_dtlb_m = perf_event_dtlb_miss();
+    int fd_l1_a = perf_event_l1d_access();
+    int fd_l1_m = perf_event_l1d_miss();
+    int fd_llc_a = perf_event_llc_access();
+    int fd_llc_m = perf_event_llc_miss();
+
+    perf_reset(fd_cycles);
+    perf_reset(fd_instrs);
+    perf_reset(fd_clock);
+    perf_reset(fd_faults);
+    perf_reset(fd_dtlb_a);
+    perf_reset(fd_dtlb_m);
+    perf_reset(fd_l1_a);
+    perf_reset(fd_l1_m);
+    perf_reset(fd_llc_a);
+    perf_reset(fd_llc_m);
+
     auto start_opt = high_resolution_clock::now();
     kernel.compute(c.data(), a.data(), b.data(), a.rows(), a.cols(), b.cols());
     auto end_opt = high_resolution_clock::now();
-    auto duration_opt = duration_cast<duration<double>>(end_opt - start_opt).count();
 
+    perf_disable(fd_cycles);
+    perf_disable(fd_instrs);
+    perf_disable(fd_clock);
+    perf_disable(fd_faults);
+    perf_disable(fd_dtlb_a);
+    perf_disable(fd_dtlb_m);
+    perf_disable(fd_l1_a);
+    perf_disable(fd_l1_m);
+    perf_disable(fd_llc_a);
+    perf_disable(fd_llc_m);
+
+    uint64_t cycles = perf_read(fd_cycles);
+    uint64_t instrs = perf_read(fd_instrs);
+    uint64_t clock = perf_read(fd_clock);
+    uint64_t faults = perf_read(fd_faults);
+    uint64_t dtlb_a = perf_read(fd_dtlb_a);
+    uint64_t dtlb_m = perf_read(fd_dtlb_m);
+    uint64_t l1_a = perf_read(fd_l1_a);
+    uint64_t l1_m = perf_read(fd_l1_m);
+    uint64_t llc_a = perf_read(fd_llc_a);
+    uint64_t llc_m = perf_read(fd_llc_m);
+
+    perf_close_event(fd_cycles);
+    perf_close_event(fd_instrs);
+    perf_close_event(fd_clock);
+    perf_close_event(fd_faults);
+    perf_close_event(fd_dtlb_a);
+    perf_close_event(fd_dtlb_m);
+    perf_close_event(fd_l1_a);
+    perf_close_event(fd_l1_m);
+    perf_close_event(fd_llc_a);
+    perf_close_event(fd_llc_m);
+
+    auto seconds = duration_cast<duration<double>>(end_opt - start_opt).count();
     double M = static_cast<double>(a.rows());
     double N = static_cast<double>(b.cols());
     double K = static_cast<double>(a.cols());
-    double operations = 2.0 * M * N * K;
-    double gflops = operations / (duration_opt * 1e9);
+    double gflops = (2.0 * M * N * K) / (seconds * 1e9);
 
-    printf("ok | time = %.6e s | GFLOPS = %.2f\n", duration_opt, gflops);
+    printf("ok | time = %.6f s | GFLOPS = %.2f\n", seconds, gflops);
+    printf("task-clock     = %lu\n", clock);
+    printf("cycles         = %lu\n", cycles);
+    printf("instructions   = %lu\n", instrs);
+    printf("- IPC          = %f\n", (double)instrs / cycles);
+    printf("page-faults    = %lu\n", faults);
+    printf("dTLB access    = %lu, miss = %lu\n", dtlb_a, dtlb_m);
+    printf("L1D  access    = %lu, miss = %lu\n", l1_a, l1_m);
+    printf("LLC  access    = %lu, miss = %lu\n", llc_a, llc_m);
 }
 
 int main() {
--- test/tests/perf_submatmul_4kx16kx128.cpp
+++ test/tests/perf_submatmul_4kx16kx128.cpp
@@ -4,6 +4,7 @@
 
 #include "core/matmul.h"
 #include "matrix.hpp"
+#include "perf.h"
 
 void test(int m, int k, int n) {
     using namespace std::chrono;
@@ -15,6 +16,28 @@ void test(int m, int k, int n) {
     const int M_BLK = 128;
     const int K_BLK = 256;
     const int N_BLK = 128;
+
+    int fd_cycles = perf_event_cycles();
+    int fd_instrs = perf_event_instructions();
+    int fd_clock = perf_event_task_clock();
+    int fd_faults = perf_event_page_faults();
+    int fd_dtlb_a = perf_event_dtlb_access();
+    int fd_dtlb_m = perf_event_dtlb_miss();
+    int fd_l1_a = perf_event_l1d_access();
+    int fd_l1_m = perf_event_l1d_miss();
+    int fd_llc_a = perf_event_llc_access();
+    int fd_llc_m = perf_event_llc_miss();
+
+    perf_reset(fd_cycles);
+    perf_reset(fd_instrs);
+    perf_reset(fd_clock);
+    perf_reset(fd_faults);
+    perf_reset(fd_dtlb_a);
+    perf_reset(fd_dtlb_m);
+    perf_reset(fd_l1_a);
+    perf_reset(fd_l1_m);
+    perf_reset(fd_llc_a);
+    perf_reset(fd_llc_m);
 
     auto start_submatmul = high_resolution_clock::now();
     for (int m_idx = 0, m_block; m_idx < m; m_idx += m_block) {
@@ -31,11 +54,52 @@ void test(int m, int k, int n) {
         }
     }
     auto end_submatmul = high_resolution_clock::now();
-    auto duration_submatmul = duration_cast<duration<double>>(end_submatmul - start_submatmul);
 
-    double seconds = duration_submatmul.count();
+    perf_disable(fd_cycles);
+    perf_disable(fd_instrs);
+    perf_disable(fd_clock);
+    perf_disable(fd_faults);
+    perf_disable(fd_dtlb_a);
+    perf_disable(fd_dtlb_m);
+    perf_disable(fd_l1_a);
+    perf_disable(fd_l1_m);
+    perf_disable(fd_llc_a);
+    perf_disable(fd_llc_m);
+
+    uint64_t cycles = perf_read(fd_cycles);
+    uint64_t instrs = perf_read(fd_instrs);
+    uint64_t clock = perf_read(fd_clock);
+    uint64_t faults = perf_read(fd_faults);
+    uint64_t dtlb_a = perf_read(fd_dtlb_a);
+    uint64_t dtlb_m = perf_read(fd_dtlb_m);
+    uint64_t l1_a = perf_read(fd_l1_a);
+    uint64_t l1_m = perf_read(fd_l1_m);
+    uint64_t llc_a = perf_read(fd_llc_a);
+    uint64_t llc_m = perf_read(fd_llc_m);
+
+    perf_close_event(fd_cycles);
+    perf_close_event(fd_instrs);
+    perf_close_event(fd_clock);
+    perf_close_event(fd_faults);
+    perf_close_event(fd_dtlb_a);
+    perf_close_event(fd_dtlb_m);
+    perf_close_event(fd_l1_a);
+    perf_close_event(fd_l1_m);
+    perf_close_event(fd_llc_a);
+    perf_close_event(fd_llc_m);
+
+    auto seconds = duration_cast<duration<double>>(end_submatmul - start_submatmul).count();
+
     double gflops = (2.0 * m * k * n) / (seconds * 1e9);
     printf("ok | submatmul = %.6f s | GFLOPS = %.2f\n", seconds, gflops);
+    printf("task-clock     = %lu\n", clock);
+    printf("cycles         = %lu\n", cycles);
+    printf("instructions   = %lu\n", instrs);
+    printf("- IPC          = %f\n", (double)instrs / cycles);
+    printf("page-faults    = %lu\n", faults);
+    printf("dTLB access    = %lu, miss = %lu\n", dtlb_a, dtlb_m);
+    printf("L1D  access    = %lu, miss = %lu\n", l1_a, l1_m);
+    printf("LLC  access    = %lu, miss = %lu\n", llc_a, llc_m);
 
     free(lhs);
     free(rhs);
