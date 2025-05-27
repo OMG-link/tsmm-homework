e07c75ee748a0f4dd189cb9de4c43a3d6c686d13
OMG_link <50469469+OMG-link@users.noreply.github.com>
2025-05-27 13:43:12 +0800

    opt: optmize generic kernel

--- include/core/matmul.h
+++ include/core/matmul.h
@@ -9,7 +9,7 @@ extern "C" {
 void matmul_block(f64 *RESTRICT dst, const f64 *RESTRICT lhs, const f64 *RESTRICT rhs, int m, int k, int n,
                   const int M_BLK, const int K_BLK, const int N_BLK);
 void matmul_submat(f64 *RESTRICT dst, const f64 *RESTRICT lhs, const f64 *RESTRICT rhs, int m, int k, int n,
-                   int dst_line_stride);
+                   int dst_line_stride, int zero_dst);
 
 #ifdef __cplusplus
 }
--- test/tests/perf_matmul_4kx128x16k.cpp
+++ test/tests/perf_matmul_4kx128x16k.cpp
@@ -18,7 +18,7 @@ void test(const Matrix &a, const Matrix &b) {
     test_kernel(
         a, b,
         [](f64 *dst, const f64 *lhs, const f64 *rhs, size_t m, size_t k, size_t n) {
-            matmul_block(dst, lhs, rhs, m, k, n, 192, 256, 192);
+            matmul_block(dst, lhs, rhs, m, k, n, 4000, 128, 720);
         },
         "generic-optimized");
 }
